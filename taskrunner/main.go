e49b06b7d75e2827298e901d43f6cd9f02fe936e
vcollado <necseron@gmail.com>
2017-06-04 23:53:49 +0200

    added nil calls

--- taskrunner/main.go
+++ taskrunner/main.go
@@ -26,4 +26,14 @@ func main() {
 
 	// acronym
 	fmt.Println(tasks.Acronym("I Am Your Gopher!"))
+
+	// Nil
+	p1 := "Ey Nil, bon dia"
+	fmt.Println(p1, tasks.Nil(p1))
+	p2 := "Saps que ens faran fora del pis si no paguem les factures?"
+	fmt.Println(p2, tasks.Nil(p2))
+	p3 := "Perdoni, voster és el llogater d'aquesta finca no?!"
+	fmt.Println(p3, tasks.Nil(p3))
+	p4 := ""
+	fmt.Println(p4, tasks.Nil(p4))
 }
