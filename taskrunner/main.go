d0b20a21e0dbf19bde1b56247ef17af20c4a1c07
vcollado <necseron@gmail.com>
2017-06-04 17:55:13 +0200

    add call to accumulate

--- taskrunner/main.go
+++ taskrunner/main.go
@@ -9,9 +9,18 @@ import (
 func main() {
 	var dna1, dna2 string = "GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT"
 
+	// hamming
 	hammingDistance := tasks.Hamming(dna1, dna2)
-
 	fmt.Println(dna1)
 	fmt.Println(dna2)
 	fmt.Println(hammingDistance)
+	// output:
+	// GAGCCTACTAACGGGAT
+	// CATCGTAATGACGGCCT
+	// ^ ^ ^  ^ ^    ^^
+
+	// accumulator
+	fmt.Println(tasks.Accumulate([]int{25, 5, 3}, tasks.Square))
+	// output:
+	// [625 25 9]
 }
